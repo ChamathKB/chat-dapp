6316dfbeb8c0091c80060ce5ff55c52ddfb1b838
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2022-07-01 20:15:06 +0530

    created user functionalities

---
+++ src/user.js
@@ -0,0 +1,20 @@
+import GUN from 'gun';
+import 'gun/sea';
+import 'gun/axe';
+import { writeable } from 'svelte/store';
+
+// Database
+export const db = GUN();
+
+// Gun user
+export const user = db.user().recall({sessionStorage: true});
+
+// Curent user's username
+export const username = writeable('');
+
+user.get('alias').on(v => username.set(v))
+
+db.on('auth', async(event) => {
+    const alias = await user.get('alias'); // username string
+    username.set(alias);     
+});
